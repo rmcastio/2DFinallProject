03e0e90f36c31c5799f66227eb61f23343467646
RichardCastillo <armed_deity@hotmail.com>
2020-05-12 06:12:39 -0700

    Adding classes and methods

--- src/edu/whccd/rcastillo/Main.java
+++ src/edu/whccd/rcastillo/Main.java
@@ -15,29 +15,43 @@ import java.util.ArrayList;
 
 
 
+
+
 public class Main
 {
 
+
+	public static void displayMenu()
+	{
+		System.out.println("Make a Profile (1)");
+		System.out.println("Edit Profile (2)");
+		System.out.println("Find a Date (3)");
+		System.out.print("Exit (4)");
+	}
+
+
 	public static void main(String[] args)
 	{
-		Profile user = new Profile();
 
-		Boolean exitProgram = false;
+
+		Boolean exitProgram = false; // Bool Variable to exit While Loop.
+
+
+		// Title Screen
+		System.out.print("Welcome to the Binary love connection form.");
+		System.out.println();
+		System.out.println("<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3");
+		System.out.println();
+
+		int option;
 
 		while (!exitProgram) // Main Event Loop
 		{
-			System.out.print("Welcome to the Binary love connection form.");
-			System.out.println();
-			System.out.println("<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3");
-			System.out.println();
+			displayMenu();
+
+			makeProfile();
 
 
-			Scanner s1 = new Scanner(System.in);
-			System.out.print("Enter First Name: ");
-			user.firstName = s1.next();
-			System.out.print("Enter Last Name: ");
-			user.lastName = s1.next();
-			System.out.println("Hello " + user.lastName + ", " + user.firstName);
 
 		}
 
@@ -46,6 +60,17 @@ public class Main
 
 	}
 
+	public static void makeProfile()
+	{
+		Profile user = new Profile();
+		Scanner s1 = new Scanner(System.in);
+
+		System.out.print("Enter First Name: ");
+		user.firstName = s1.next();
+		System.out.print("Enter Last Name: ");
+		user.lastName = s1.next();
+		System.out.println("Hello " + user.lastName + ", " + user.firstName);
+	}
 
 }
 
